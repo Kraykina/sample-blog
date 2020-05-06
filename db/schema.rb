f88859313db123896c5a03c45e0ede25310bcb05
Olga Kraykina <olga.kraykina904@gmail.com>
2020-05-06 21:11:29 +0100

    Add username column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_02_104721) do
+ActiveRecord::Schema.define(version: 2020_05_06_200057) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2020_05_02_104721) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index [nil], name: "index_users_on_username", unique: true
   end
 
 end
